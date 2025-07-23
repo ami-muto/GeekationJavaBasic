5ed54ea648916d859ef763f3925196fc40187935
ami-muto <a.muto.cingroup2012@gmail.com>
2025-07-24 03:07:25 +0900

    クラスの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,13 @@
+package basicSyntax;
+
+public class Main{
+ public static void main(String[] args) {
+	int age =20;
+	double height =158.2;
+	 GeekIntroduction. greeting(age,height);
+	
+	int num1 =2;
+	int num2 =300;
+	 GeekIntroduction. specialSkill(num1,num2);
+}
+}
